2044d1804c824bd1bda4c229ace2ddf6ed215131
zzzhizhi <77013105+zzzhizhia@users.noreply.github.com>
2025-01-30 22:10:01 +0800

    style: simplify Image component formatting in page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import React from 'react'
 import { useUWU } from '~/hooks/useUWU'
 import { useTranslations } from 'next-intl'
 import Image from 'next/image'
@@ -16,11 +15,7 @@ export default function Home() {
     <>
       <h1>👋&nbsp;{t('Heading')}</h1>
       <p>{t.rich('Paragraph1')}</p>
-      <Image
-        src={isUWU ? UwU : Campus}
-        alt="XKU Campus"
-        placeholder="blur"
-      />
+      <Image src={isUWU ? UwU : Campus} alt="XKU Campus" placeholder="blur" />
       <Image src={SicoBoom} alt="Sico Boom" placeholder="blur" />
       <p className="-mt-4 text-center text-sm text-stone-400 dark:text-stone-500">
         {t('Caption')}
